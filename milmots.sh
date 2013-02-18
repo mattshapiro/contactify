f775c4e2c3b7d2372e5811064761aa7b2f3e2393
mattshapiro <typorrhea@gmail.com>
2013-02-18 14:35:39 -0700

    Fixing my script so the kids don't make fun of me

--- milmots.sh
+++ milmots.sh
@@ -6,8 +6,8 @@ do
 	if [[ $count -lt 1000 ]]
 	then
 		rand32=$((( ($RANDOM) << 16) | ($RANDOM) ))
-		num=$((`wc -l < /usr/share/dict/words` + 1))
-		word=`head -$(($rand32 % $num + 1)) /usr/share/dict/words | tail -1`
+		num=$((`wc -l < /usr/share/dict/words`))
+		word=`head -$(($rand32 % $num)) /usr/share/dict/words | tail -1`
 		echo "<li>"$word"</li>"
 	else
 		break
